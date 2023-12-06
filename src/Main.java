dc499c42ca45669e90f0bf757ce5622da79bd140
LuizSoghomonyan <luiza.soxhomonyan@gmail.com>
2023-12-06 18:09:13 +0400

    Strings: Z function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import strings.zFunction.zFunction;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 class Main
 {
     public static void main (String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        zFunction zFunc = new zFunction();
+        System.out.println(zFunc.getZFunctionArray(s));
     }
 
 }
--- src/strings/zFunction/zFunction.java
+++ src/strings/zFunction/zFunction.java
@@ -1,4 +1,32 @@
 package strings.zFunction;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
 public class zFunction {
+    public ArrayList<Integer> getZFunctionArray(String str) {
+        ArrayList<Integer> zFunc = new ArrayList<>(Collections.nCopies(str.length(), 0));
+        int L = 0, R = 0;
+        for (int i = 1; i < str.length(); i++) {
+            if (i <= R) {
+                zFunc.set(i, Math.min(R - i + 1, zFunc.get(i - L)));
+            }
+
+            while (i + zFunc.get(i) < str.length()
+            && str.charAt(zFunc.get(i)) == str.charAt(i + zFunc.get(i))){
+                zFunc.set(i, zFunc.get(i) + 1);
+            }
+            if (i + zFunc.get(i) - 1 > R) {
+                R = i + zFunc.get(i) - 1;
+                L = i;
+            }
+
+
+
+        }
+
+        return zFunc;
+    }
 }
+//aabcaabacaabc
